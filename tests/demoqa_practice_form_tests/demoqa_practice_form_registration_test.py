16e2a5b6ffa9896531e1c150ae8b65387b3ae06c
div50015 <div50015@mail.ru>
2026-09-17 15:52:26 +0300

    Add dir demoqa_practice_form_tests and file test_demoqa_practice_form_registration

---
+++ tests/demoqa_practice_form_tests/demoqa_practice_form_registration_test.py
@@ -0,0 +1,13 @@
+import pytest
+from selene import browser, have, command
+from time import sleep
+import os.path
+
+def test_demoqa_practice_form_registration():
+    # GIVEN
+    browser.open('https://demoqa.com/automation-practice-form')
+    pass
+
+    # WHEN
+
+    # THEN
