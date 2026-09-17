8ef197e931c9d9b199286ebbfdbe8b9a3f1465f3
div50015 <div50015@mail.ru>
2026-09-17 15:52:26 +0300

    Edit .gitignore and tests_temp.py files.

--- tests/tests_temp.py
+++ tests/tests_temp.py
@@ -1,5 +1,10 @@
+from time import sleep
 import pytest
-
+from selene import browser
+import time
 
 def test_tmp():
+    browser.open('https://www.google.com')
+    sleep(5)
+
     print('\n Тест прошел успешно \n*************************************')
