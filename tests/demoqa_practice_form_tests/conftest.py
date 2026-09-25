8a602c53616de7953919af3481c24433330908e8
Igor Degtyarenko <div50015@mail.ru>
2026-09-25 18:41:30 +0300

    Completed of test editing and add conftest.py file

---
+++ tests/demoqa_practice_form_tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope='function', autouse=True)
+def open_browser():
+    browser.config.window_width = 1000
+    browser.config.window_height = 1100
+    browser.config.base_url = 'https://demoqa.com'
+
+
+    yield
+
+    browser.quit()
--- tests/demoqa_practice_form_tests/demoqa_practice_form_registration_test.py
+++ tests/demoqa_practice_form_tests/demoqa_practice_form_registration_test.py
@@ -5,44 +5,48 @@ from selene import browser, have, command
 from time import sleep
 import os.path
 from selenium.webdriver import Keys
+import tests
 
 
 def test_demoqa_practice_form_registration():
     # GIVEN
-    browser.config.window_width = 1000
-    browser.config.window_height = 1100
-    browser.open('https://demoqa.com/automation-practice-form')
+
+    browser.open('/automation-practice-form')
     browser.driver.execute_script("document.querySelector('.body-height').style.transform='scale(.65)'")
 
     # WHEN
 
     browser.element('#firstName').type('ИГОРЬ')
-    browser.element('#lastName').type('ПЕТРОВ')
-    browser.element('#userEmail').type('div50015@mail.ru')
+    browser.element('#lastName').type('Д')
+    browser.element('#userEmail').type('div@mail.ru')
     browser.element('[value=Male][name=gender]').click()
     browser.element('#userNumber').type('9999999991')
-    browser.element('#dateOfBirthInput').send_keys(Keys.CONTROL, 'a').type('4 Aug 1967').press_enter()
-    pass
+    browser.element('#dateOfBirthInput').send_keys(Keys.CONTROL, 'a').type('10 Aug 1999').press_enter()
     browser.element('#subjectsInput').type('His').press_enter()
     browser.all('[id^=hobbies-checkbox]')[0].click()
-
     # browser.element('#uploadPicture').set_value(
     #     str(Path(tests.__file__).parent.joinpath('demoqa_pactice_form_tests/resources/foto.jpg').absolute())
-    import tests
-
     browser.element("#uploadPicture").send_keys(
         os.path.dirname(tests.__file__), "/demoqa_practice_form_tests/photo.jpg"
     )
-
-    browser.element('#currentAddress').type('Moskovskaya strit 10')
-
+    browser.element('#currentAddress').type('Moskovskaya Street 10')
     browser.element("#state").click()
     browser.all("[id^=react-select][id*=option]").element_by(have.text("NCR")).click()
     browser.element("#city").click()
     browser.all("[id^=react-select][id*=option]").element_by(have.text("Delhi")).click()
-
     browser.element("#submit").click()
-    pass
-
 
     # THEN
+
+    browser.element('.table').all('td').should(have.texts(
+        'Student Name', 'ИГОРЬ Д',
+        'Student Email', 'div@mail.ru',
+        'Gender', 'Male',
+        'Mobile', '9999999991',
+        'Date of Birth', '10 August,1999',
+        'Subjects', 'History',
+        'Hobbies', 'Sports',
+        'Picture', 'photo.jpg',
+        'Address', 'Moskovskaya Street 10',
+        'State and City', 'NCR Delhi'
+    ))
